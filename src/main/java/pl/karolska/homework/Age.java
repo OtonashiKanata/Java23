bca025b972ce5c5651650dd942da553aca3daab2
Justyna Karolska <justyna.karolska@gmail.com>
2021-08-28 11:37:31 +0200

    Some changes

---
+++ src/main/java/pl/karolska/homework/Age.java
@@ -0,0 +1,21 @@
+package pl.karolska.homework;
+
+public class Age {
+
+    public static String ageVerification(int age1) {
+        if (age1 >= 18) {
+            return "You are over 18";
+
+        } else {
+
+            return "You are below 18";
+        }
+
+    }
+    public static void main (String[]args){
+
+        System.out.println(ageVerification(12));
+
+    }
+
+}
