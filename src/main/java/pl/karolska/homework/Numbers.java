696e1ab1dafd9294beed40181291e7a460bc2d09
Justyna Karolska <justyna.karolska@gmail.com>
2021-09-11 01:42:40 +0200

    Shorthened method and changed names

--- src/main/java/pl/karolska/homework/Numbers.java
+++ src/main/java/pl/karolska/homework/Numbers.java
@@ -2,32 +2,23 @@ package pl.karolska.homework;
 
 public class Numbers {
 
-    public static boolean isTheNumberEven(int num1) {
+    public static boolean isTheNumberEven(int num1){
 
-        if (num1 % 2 == 0) {
-            return true;
-      }
-    else {
-        return false;
+        return num1 % 2 == 0;
+
         }
-    }
 
-    public static boolean isTheNumberOdd(int num1){
+        public static boolean isTheNumberOdd(int num1){
 
-        if (num1 % 2 != 0) {
-             return true;
-    }
-    else {
-        return false;
+            return num1 % 2 != 0;
+
         }
-    }
+
+        public static void main (String[]args ){
+
+         System.out.println(isTheNumberEven(8));
+         System.out.println(isTheNumberOdd(8));
 
 
-    public static void main( String[] args ){
-
-        System.out.println(isTheNumberEven(8));
-        System.out.println(isTheNumberOdd(8));
-
-
-    }
-}
+            }
+        }
