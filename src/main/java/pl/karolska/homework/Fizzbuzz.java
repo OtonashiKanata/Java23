819d37c317d7fe24605f9f00f6d863da63d44fc8
Justyna Karolska <justyna.karolska@gmail.com>
2021-09-12 23:11:21 +0200

    Added game fizzbuzz

---
+++ src/main/java/pl/karolska/homework/Fizzbuzz.java
@@ -0,0 +1,28 @@
+package pl.karolska.homework;
+
+
+public class Fizzbuzz {
+
+    public static void gameOfFizzbuzz() {
+
+        for (int i = 1; i <= 100; i++) {
+
+            if (i % 3 == 0 && i % 5 == 0)
+                System.out.println("Fizzbuzz");
+            else if (i % 3 == 0)
+                System.out.println("Fizz");
+            else if (i % 5 == 0)
+                System.out.println("Buzz");
+            else
+                System.out.println(i);
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        Fizzbuzz.gameOfFizzbuzz();
+
+    }
+}
