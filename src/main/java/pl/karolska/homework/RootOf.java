9e86c56026c4950496b53654151875e219370069
Justyna Karolska <justyna.karolska@gmail.com>
2021-09-24 20:37:19 +0200

    Small change

--- src/main/java/pl/karolska/homework/RootOf.java
+++ src/main/java/pl/karolska/homework/RootOf.java
@@ -2,7 +2,7 @@ package pl.karolska.homework;
 
 public class RootOf {
 
-    public static double RootOfANumber(double num1) {
+    public static double calculateRootOfANumber(double num1) {
         if (num1 < 0) {
             throw new IllegalArgumentException();
         }
@@ -11,9 +11,9 @@ public class RootOf {
 
     public static void main(String[] args) {
 
-        System.out.println(RootOfANumber(9));
-        System.out.println(RootOfANumber(5));
-        System.out.println(RootOfANumber(-5));
+        System.out.println(calculateRootOfANumber(9));
+        System.out.println(calculateRootOfANumber(5));
+        System.out.println(calculateRootOfANumber(-5));
 
     }
 }
