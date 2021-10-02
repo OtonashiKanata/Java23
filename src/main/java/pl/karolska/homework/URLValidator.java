c07378f79b2c9423cd20bc56c9928c38bbea33e9
Justyna Karolska <justyna.karolska@gmail.com>
2021-10-02 21:22:02 +0200

    Added a URL validator

--- src/main/java/pl/karolska/homework/URLValidator.java
+++ src/main/java/pl/karolska/homework/URLValidator.java
@@ -7,14 +7,19 @@ public class URLValidator {
 
     public static void main(String[] args) {
         Pattern pattern = Pattern.compile("^(https?://)?(www\\.)?([\\w]+\\.)+[\\w]{2,3}$");
-        Matcher matcher1 = pattern.matcher("http://www.google.pl");
-        Matcher matcher2 = pattern.matcher("http://www.filmweb.pl");
-        Matcher matcher3 = pattern.matcher("http://www.youtube.");
-
+        Matcher matcher1 = pattern.matcher("http://www.onet.pl");
+        Matcher matcher2 = pattern.matcher("https://mail.google.com");
+        Matcher matcher3 = pattern.matcher("http://wiadmosci.onet.pl");
+        Matcher matcher4 = pattern.matcher("http://onet.pl");
+        Matcher matcher5 = pattern.matcher("www.onet.pl");
+        Matcher matcher6 = pattern.matcher("www. onet.pl");
 
         System.out.println(matcher1.matches());
         System.out.println(matcher2.matches());
         System.out.println(matcher3.matches());
+        System.out.println(matcher4.matches());
+        System.out.println(matcher5.matches());
+        System.out.println(matcher6.matches());
 
     }
 }
